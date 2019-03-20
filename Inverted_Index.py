74d5c3e09b14cefeb19d309ad2f4ba7a211f6331
alireza parchami <alirezaprm76@gmail.com>
2019-03-20 21:06:40 +0330

    first stable version

--- Inverted_Index.py
+++ Inverted_Index.py
@@ -1,10 +1,11 @@
 from nltk.stem import PorterStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
 from collections import OrderedDict
+
 frequent_words = set()
 words = dict()
 ps = PorterStemmer()
-
+number_of_doc = 0
 def read_common_words():
     f = open("frequent.txt", "r")
     for x in f:
@@ -23,7 +24,6 @@ def append_docId_pos(docId, indices, docID_pos_dic):
 
 
 def read_docs():
-    global number_of_doc
     for i in range(1, number_of_doc + 1):
         st = str(i) + ".txt"
         my_file = open(st, "r")
@@ -92,7 +92,7 @@ def NOT(t1 , op, t2):
     t1 = set(t1)
     t2 = ps.stem(t2)
     doc1 = set(words[t2].keys())
-    result_t2 = set(range(1, number_of_doc+1)).difference(doc1)
+    result_t2 = set(range(1, number_of_doc + 1)).difference(doc1)
     if op == "AND":
         result = t1 & result_t2
     elif op == "OR":
@@ -186,96 +186,3 @@ def union2(t1,t2):
     doc2 = set(words[t2].keys())
     t1 = set(t1)
     return t1 | doc2
-
-
-def query():
-    # alireza AND friend
-    t1 = "alireza"
-    t2 = "friend"
-    q1 = intersect(t1, t2)
-
-    # alireza AND apologise
-    t1 = "alireza"
-    t2 = "apologise"
-    q2 = intersect(t1, t2)
-
-    # proud AND friend AND responsible
-    t1 = "proud"
-    t2 = "friend"
-    t3 = "responsible"
-    q3 = intersect(t1, t2, t3)
-
-    # apologise OR mistake
-    t1 = "apologise"
-    t2 = "mistake"
-    q4 = union(t1,t2)
-
-    # years OR Hector OR NOT problems
-    t1 = "years"
-    t2 = "Hector"
-    t3 = "problems"
-    q5 = NOT(union(t1, t2), "OR", t3)
-
-    # NOT apologise OR glad AND NOT meet
-    t1 = "apologise"
-    t2 = "glad"
-    t3 = "meet"
-    q6 = NOT(NOT(union(t2), "OR", t1), "AND", t3)
-
-    # my OR best AND friend
-    t1 = "my"
-    t2 = "best"
-    t3 = "friend"
-    q7 = intersect2(union(t1,t2), t3)
-
-    # my AND NOT his OR friend
-    t1 = "my"
-    t2 = "his"
-    t3 = "friend"
-    q8 = union2(NOT(t2, "AND", t1), t3)
-
-    # let with me with know
-    t1 = "let"
-    t2 = "me"
-    t3 = "know"
-    q9 = with_op(t1, t2, t3)
-
-    # please NEAR 2 know NEAR 1 it
-    t1 = "please"
-    t2 = "know"
-    t3 = "it"
-    q10 = near(2,t1,t2,1,t3)
-
-    # twenty NEAR 2 old
-    t1 = "twenty"
-    t2 = "old"
-    q11 = near(2, t1, t2)
-
-    # responsible WITH for NEAR 2 problems
-    t1 = "responsible"
-    t2 = "for"
-    t3 = "problems"
-    q12 = near(0,t1,t2,2,t3)
-
-    # twenty NEAR 1 years with old
-    t1 = "twenty"
-    t2 = "years"
-    t3 = "old"
-    q13 =  near(1,t1,t2,0,t3)
-
-
-
-number_of_doc = int(input("Enter Number of docs:"))
-read_common_words()
-read_docs()
-words = OrderedDict(sorted(words.items(), key=lambda t: t[0]))
-print(words)
-# query_numbers = 7
-# print(query_numbers)
-# read_queries(query_numbers)
-# intersect(t1, t2)
-# union(t1,t2)
-# NOT(t2)
-# with_op(t1, t2, t3)
-#print(near(1,t1,t3))
-query()
--- queries.py
+++ queries.py
@@ -1,67 +1,113 @@
-import main as m
+import Inverted_Index as invInd
+from collections import OrderedDict
 def query():
-    t1 = ""
-    t2 = ""
-    t3 = ""
-
     # alireza AND friend
     t1 = "alireza"
     t2 = "friend"
-    q1 = m.intersect(t1, t2)
+    q1 = invInd.intersect(t1, t2)
 
     # alireza AND apologise
     t1 = "alireza"
     t2 = "apologise"
-    q2 = m.intersect(t1, t2)
+    q2 = invInd.intersect(t1, t2)
 
     # proud AND friend AND responsible
     t1 = "proud"
     t2 = "friend"
     t3 = "responsible"
-    q3 = m.intersect(t1, t2, t3)
+    q3 = invInd.intersect(t1, t2, t3)
 
     # apologise OR mistake
     t1 = "apologise"
     t2 = "mistake"
-    q4 = m.union(t1,t2)
+    q4 = invInd.union(t1,t2)
 
     # years OR Hector OR NOT problems
     t1 = "years"
     t2 = "Hector"
     t3 = "problems"
-    q5 = m.NOT(m.union(t1, t2), "OR", t3)
+    q5 = invInd.NOT(invInd.union(t1, t2), "OR", t3)
 
     # NOT apologise OR glad AND NOT meet
     t1 = "apologise"
     t2 = "glad"
     t3 = "meet"
-    q6 = m.NOT(m.NOT(m.union(t2), "OR", t1), "AND", t3)
+    q6 = invInd.NOT(invInd.NOT(invInd.union(t2), "OR", t1), "AND", t3)
 
     # my OR best AND friend
     t1 = "my"
     t2 = "best"
     t3 = "friend"
-    q7 = m.intersect2(m.union(t1,t2), t3)
+    q7 = invInd.intersect2(invInd.union(t1,t2), t3)
 
     # my AND NOT his OR friend
     t1 = "my"
     t2 = "his"
     t3 = "friend"
-    q8 = m.union2(m.NOT(t2, "AND", t1), t3)
+    q8 = invInd.union2(invInd.NOT(t2, "AND", t1), t3)
 
     # let with me with know
     t1 = "let"
     t2 = "me"
     t3 = "know"
-    q9 = m.with_op(t1, t2, t3)
+    q9 = invInd.with_op(t1, t2, t3)
 
     # please NEAR 2 know NEAR 1 it
     t1 = "please"
     t2 = "know"
     t3 = "it"
-    q10 = m.near(2,t1,t2,1,t3)
+    q10 = invInd.near(2,t1,t2,1,t3)
 
     # twenty NEAR 2 old
     t1 = "twenty"
     t2 = "old"
-    q11 = m.near(2, t1, t2)
+    q11 = invInd.near(2, t1, t2)
+
+    # responsible WITH for NEAR 2 problems
+    t1 = "responsible"
+    t2 = "for"
+    t3 = "problems"
+    q12 = invInd.near(0,t1,t2,2,t3)
+
+    # twenty NEAR 1 years WITH old
+    t1 = "twenty"
+    t2 = "years"
+    t3 = "old"
+    q13 = invInd.near(1,t1,t2,0,t3)
+
+    # let NEAR 1 know AND Hector
+    t1 = "let"
+    t2 = "know"
+    t3 = "hector"
+    q14 = invInd.intersect2(invInd.near(1,t1,t2),t3)
+
+    # best WITH friend OR Hector
+    t1 = "best"
+    t2 = "friend"
+    t3 = "Hector"
+    q15 = invInd.union2(invInd.with_op(t1,t2),t3)
+
+    print(q1)
+    print(q2)
+    print(q3)
+    print(q4)
+    print(q5)
+    print(q6)
+    print(q7)
+    print(q8)
+    print(q9)
+    print(q10)
+    print(q11)
+    print(q12)
+    print(q13)
+    print(q14)
+    print(q15)
+
+
+words = invInd.words
+invInd.number_of_doc = int(input("Enter Number of docs:"))
+invInd.read_common_words()
+invInd.read_docs()
+words = OrderedDict(sorted(words.items(), key=lambda t: t[0]))
+print(words)
+query()
